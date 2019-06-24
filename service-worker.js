ded0a4b2af6366442004257163796614d5f325a7
tiennh1995 <tiennh1995@users.noreply.github.com>
2019-06-24 23:39:59 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -16,18 +16,24 @@ self.addEventListener('install', event => {
 });
 
 self.addEventListener('fetch', function(event) {
-	event.respondWith(
-		caches.open(staticCacheName).then(function(cache) {
-  		return cache.match(event.request).then(function (response) {
-    		return response || fetch(event.request).then(function(response) {
-    			cache.put(event.request, response.clone());
-    			if(response['url'].includes('json')) {
-    				return response.json();
-    			} else {
-    				return response;
-    			}
-    		});
-  		});
-  	})
-	);
+	event.respondWith(caches.open(staticCacheName).then(function(cache) {
+		return cache.match(event.request).then(function (response) {
+			if(response) {
+				if(response['url'].includes('json')) {
+  				return response.json();
+  			} else {
+  				return response;
+  			}
+			} else {
+				fetch(event.request).then(function(response) {
+  				cache.put(event.request, response.clone());
+  				if(response['url'].includes('json')) {
+  					return response.json();
+  				} else {
+  					return response;
+  				}
+				});
+			}
+		});
+	});
 });
