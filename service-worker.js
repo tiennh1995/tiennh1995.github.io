0e638f41086ef36f7602344011c6d1c8904f6496
tiennh1995 <tiennh1995@users.noreply.github.com>
2019-06-24 21:54:53 +0700

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,8 @@
+self.addEventListener('install', event => {
+  console.log('Service worker installing...');
+  // Add a call to skipWaiting here
+});
+
+self.addEventListener('activate', event => {
+  console.log('Service worker activating...');
+});
